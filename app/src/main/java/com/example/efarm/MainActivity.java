32b61f1130c10a24f19bab8eba107c26933b2232
NehalH <nhosalikar@gmail.com>
2023-06-08 00:16:31 +0530

    Add browse products feature

--- app/src/main/java/com/example/efarm/MainActivity.java
+++ app/src/main/java/com/example/efarm/MainActivity.java
@@ -1,72 +1,178 @@
 package com.example.efarm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button searchButton;
-    private EditText searchEditText;
-    private TextView profileTextView, sellTextView, homeTextView;
+    private RecyclerView recyclerView;
+    private FirestoreAdapter adapter;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize views
-        searchButton = findViewById(R.id.searchButton);
-        searchEditText = findViewById(R.id.searchEditText);
-        profileTextView = findViewById(R.id.profileTextView);
-        sellTextView = findViewById(R.id.sellTextView);
-        homeTextView = findViewById(R.id.homeTextView);
+        // Initialize Firestore
+        firestore = FirebaseFirestore.getInstance();
 
-        // Set click listener for search button
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Perform search based on the entered text
-                String searchText = searchEditText.getText().toString().trim();
-                // Implement your search logic here
-                Toast.makeText(MainActivity.this, "Searching for: " + searchText, Toast.LENGTH_SHORT).show();
-            }
-        });
+        // Initialize RecyclerView
+        recyclerView = findViewById(R.id.recyclerView);
 
-        // Set click listener for profile
+        // Set layout manager
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Create and set the adapter
+        adapter = new FirestoreAdapter();
+        recyclerView.setAdapter(adapter);
+
+        // Retrieve data from Firestore and populate the adapter
+        retrieveDataFromFirestore();
+
+        // Set click listeners for navigation
+        TextView profileTextView = findViewById(R.id.profileTextView);
         profileTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Open ProfileActivity
-                Intent profileIntent = new Intent(MainActivity.this, ProfileActivity.class);
-                startActivity(profileIntent);
+                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                finish();
             }
         });
 
-        // Set click listener for sell
+        TextView sellTextView = findViewById(R.id.sellTextView);
         sellTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Open SellActivity
-                Intent sellIntent = new Intent(MainActivity.this, SellActivity.class);
-                startActivity(sellIntent);
+                startActivity(new Intent(MainActivity.this, SellActivity.class));
+                finish();
             }
         });
+    }
 
-        // Set click listener for home
-        homeTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    private void retrieveDataFromFirestore() {
+        firestore.collection("users").get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        // List to hold the retrieved documents
+                        List<QueryDocumentSnapshot> documentList = new ArrayList<>();
 
+                        // Iterate through the documents and add them to the list
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            documentList.add(document);
+                        }
+
+                        // Retrieve data from secondary collections for each document
+                        retrieveDataFromSecondaryCollections(documentList);
+                    } else {
+                        // Failed to retrieve documents
+                        // Handle the error
+                    }
+                });
+    }
+
+    private void retrieveDataFromSecondaryCollections(List<QueryDocumentSnapshot> documentList) {
+        // Iterate through the documents
+        for (QueryDocumentSnapshot document : documentList) {
+            // Retrieve the reference to the secondary collection for the current document
+            CollectionReference secondaryCollectionRef = document.getReference().collection("products");
+
+            // Retrieve documents from the secondary collection
+            secondaryCollectionRef.get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            // List to hold the retrieved secondary documents
+                            List<QueryDocumentSnapshot> secondaryDocumentList = new ArrayList<>();
+
+                            // Iterate through the secondary documents and add them to the list
+                            for (QueryDocumentSnapshot secondaryDocument : task.getResult()) {
+                                secondaryDocumentList.add(secondaryDocument);
+                            }
+
+                            // Update the data in the adapter
+                            adapter.setData(secondaryDocumentList);
+                        } else {
+                            // Failed to retrieve secondary documents
+                            // Handle the error
+                        }
+                    });
+        }
+    }
+
+    private class FirestoreAdapter extends RecyclerView.Adapter<FirestoreAdapter.ViewHolder> {
+
+        private List<QueryDocumentSnapshot> data;
+
+        public FirestoreAdapter() {
+            data = new ArrayList<>();
+        }
+
+        public void setData(List<QueryDocumentSnapshot> data) {
+            this.data = data;
+            notifyDataSetChanged();
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            QueryDocumentSnapshot document = data.get(position);
+            Product product = document.toObject(Product.class);
+
+            // Customize the view holder content based on the Product object
+            holder.productName.setText(product.getProductName());
+            holder.productCategory.setText(product.getCategory());
+            holder.productQuantity.setText(String.valueOf(product.getQuantity()));
+            holder.productPrice.setText(String.valueOf(product.getPrice()));
+
+            // Load the product image using a library like Glide or Picasso
+            // For example, using Glide:
+            // Glide.with(holder.itemView.getContext()).load(product.getImageUrl()).into(holder.productImage);
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            ImageView productImage;
+            TextView productName;
+            TextView productCategory;
+            TextView productQuantity;
+            TextView productPrice;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                productImage = itemView.findViewById(R.id.productImage);
+                productName = itemView.findViewById(R.id.productName);
+                productCategory = itemView.findViewById(R.id.productCategory);
+                productQuantity = itemView.findViewById(R.id.productQuantity);
+                productPrice = itemView.findViewById(R.id.productPrice);
             }
-        });
+        }
     }
 }
