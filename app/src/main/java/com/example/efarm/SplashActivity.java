8803e300eb28473080cc5ba27b7990972919dfd3
NehalH <nhosalikar@gmail.com>
2023-06-09 10:19:03 +0530

    Add SplashScreen

--- app/src/main/java/com/example/efarm/SplashActivity.java
+++ app/src/main/java/com/example/efarm/SplashActivity.java
@@ -20,7 +20,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(iHome);
                 finish();
             }
-        },3000);
+        },2000);
 
 
     }
